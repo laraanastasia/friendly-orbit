6e13d15586d261f8db2f727cc7c1c2f22f547431
Lara <kiepsel@Air-von-Lara.fritz.box>
2023-12-10 16:55:37 +0100

    end of day commit

--- game.py
+++ game.py
@@ -9,15 +9,15 @@ pygame.display.set_caption("fast guy")
 clock = pygame.time.Clock()
 #creating text: 1. create font ; 2. write text on surface ; 3. blit text on screen
 font = pygame.font.Font('font/Pixeltype.ttf',50)
-#import image onto suface
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+#import image onto suface (convert makes the image easier for python to work with)
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 #text you want to display ; smothe edges ; color
-text_surface = font.render('Fast Guy',False,'Black')
-enemy1_surface = pygame.image.load('graphics/enemy1/snail1.png')
+text_surface = font.render('Fast Guy',False,'Black') 
+enemy1_surface = pygame.image.load('graphics/enemy1/snail1.png').convert_alpha() # _alpha to ignore the alpha part of the image
 #variable for position of enemy one to be able to animate movement
 enemy1_var_pos =600
-#run code invinitively to make gaming possible
+#run code infinitive to make gaming possible
 while True:
     # never run code without this-you wont be able to close it on its own
     for event in pygame.event.get():
@@ -29,7 +29,10 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300,50))
-    enemy1_var_pos -= 1
+    #move enemy1 location -5 with each interval
+    enemy1_var_pos -= 5
+    # if statement to make sure snail comes back 
+    if enemy1_var_pos < -100 : enemy1_var_pos = 800
     screen.blit(enemy1_surface,(enemy1_var_pos,250))
     pygame.display.update()
     #use clock.tick to set frame rate to make sure game runs consistant
