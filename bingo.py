b31441ddbe936066a84e878bf6ff1520f420e18e
Lara <kiepsel@Air-von-Lara.fritz.box>
2023-12-19 20:41:05 +0100

    -

--- bingo.py
+++ bingo.py
@@ -1,3 +1,11 @@
 import tabulate
-table = [["1", "2","3","4","5"], ["1"],["1"],["1"],["1"],]
+import random
+table = [ ["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"]]
 print(tabulate.tabulate(table,tablefmt="grid"))
+Ausdrücke = [["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["2"],["3"],["4"],]
+list=[]
+for i in range(5):
+    numer = random.choice(Ausdrücke)
+    list.append(numer)
+print(tabulate.tabulate(list,tablefmt="grid"))
+
