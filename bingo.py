57632abf10d9e44ca00e44da6440c301bd4bb0c3
Lara <kiepsel@Air-von-Lara.fritz.box>
2023-12-20 10:59:02 +0100

    -

--- bingo.py
+++ bingo.py
@@ -1,8 +1,8 @@
 import tabulate
 import random
 table = [ ["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"]]
-print(tabulate.tabulate(table,tablefmt="grid"))
-Ausdrücke = [["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["1"],["2"],["3"],["4"],]
+print(tabulate.tabulate(table,tablefmt='fancy_grid'))
+Ausdrücke = [[["So könnte auch eine Frage in der Klausur aussehen"],["Gifs werden verschickt"],["Alex bricht sich den Nacken"],["Nico lacht"]],[["Herr und Frau Lammarsch verbessern sich passiv agressiv gegenseitig"],["Lara fragt alle sich zu wiederholen"],["Pascal programmiert in jeder Vorlesung außer in Programmieren 1"],["Jannik und Alex spielen Schach"]],[["Javacode wird für mind. 20 min erklärt"],["Sitzplatzkrieg"],[""],["1"]],[["1"],["1"],["1"],["1"]],[["1"],["1"],["1"],["1"]],[["1"],["1"],["1"],["1"]]]
 list=[]
 for i in range(5):
     numer = random.choice(Ausdrücke)
