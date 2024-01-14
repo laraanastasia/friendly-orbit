91151e5379dd676e81c75ac39f6f390364a1a5b1
Lara <kiepsel@MacBook-Air-von-Lara.local>
2024-01-15 00:05:49 +0100

    tried to make slash commands

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import Final
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
-import random 
+import random
+
+import requests 
 
 #load token
 load_dotenv()
@@ -20,13 +23,11 @@ bot= commands.Bot(command_prefix="cat!",intents=intents)
 @bot.command()
 async def no(ctx):
     await ctx.send("https://tenor.com/view/no-nope-cat-cute-gif-4544032")
+
 @bot.command()
 async def yes(ctx):
     await ctx.send("https://tenor.com/view/yes-yescat-cat-nodding-nod-gif-21702876")
 
-@bot.command()
-async def scream(ctx):
-    await ctx.send("https://tenor.com/view/cry-crying-cat-crying-cat-cry-why-gif-27571714")
 
 @bot.command()
 async def hello(ctx):
@@ -41,6 +42,7 @@ async def rand(ctx):
     catgif_list= ["https://tenor.com/view/cat-gif-27443459","https://tenor.com/view/angry-cat-triggered-ahhh-meeeeoow-gif-16764869","https://tenor.com/view/goofy-cat-cat-cat-dance-cat-funny-funny-gif-13226486845730141702","https://tenor.com/view/crunch-cat-luna-eat-gif-3997701764820700221","https://tenor.com/view/chipi-chapa-dubi-dubidu-boom-gif-17214381249162738138","https://tenor.com/view/cat-gif-21186965","https://tenor.com/view/cat-huh-cat-huh-etr-gif-15332443943609734737","https://tenor.com/view/cat-apple-gif-21978863","https://tenor.com/view/no-nope-cat-cute-gif-4544032","https://tenor.com/view/yes-yescat-cat-nodding-nod-gif-21702876","https://tenor.com/view/cats-cat-bunny-shake-head-gif-24738682","https://tenor.com/view/cry-crying-cat-crying-cat-cry-why-gif-27571714",]
     i=random.choice(catgif_list)
     await ctx.send(i)
+
 @bot.command()
 async def love(ctx):
     await ctx.send("""
@@ -49,25 +51,21 @@ async def love(ctx):
 /    づ♡ 
                    """)
 
-@bot.command()
-async def list(ctx):
-    await ctx.send(""""
-    cat!no
-    cat!yes
-    cat!love
-    cat!scream
-    cat!random
-                   """)
+@bot.tree.command(name="hi")
+async def hi(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!")
     
-@bot.command()
-async def alex(ctx):
-    await ctx.send("https://tenor.com/view/put-out-the-candle-put-it-out-birthday-cat-candles-out-neatdad-gif-2956237863302866726")  
 
 #Startup
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now purring!')
+    try:
+        synced =await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
 # Incoming Messages
 #@bot.event
